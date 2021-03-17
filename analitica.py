78e34aeb02e526fab9f7753cc3445f1051a4a144
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 18:36:12 -0500

    comentario

--- analitica.py
+++ analitica.py
@@ -2,3 +2,6 @@ numero = int(input("Escriba un número positivo: "))
 if numero < 0:
     print("¡Le he dicho que escriba un número positivo!")
 print(f"Ha escrito el número {numero}")
+
+
+#describir errores
