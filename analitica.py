627501c605aa24efed354269db44f10226aca8b4
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 19:19:20 -0500

    pruebas con respecto a errores

--- analitica.py
+++ analitica.py
@@ -2,3 +2,10 @@ numero = int(input("Escriba un número positivo: "))
 if numero < 0:
     print("¡Le he dicho que escriba un número positivo!")
 print(f"Ha escrito el número {numero}")
+#pruebas con respecto a errores
+edad = int(input("¿Cuántos años tiene? "))
+if edad < 18:
+    print("Es usted menor de edad")
+else:
+    print("Es usted mayor de edad")
+print("¡Hasta la próxima!")
