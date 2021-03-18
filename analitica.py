c56929e06c7dcd1abafa9f89a7ef602080c657a5
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 19:55:04 -0500

    comentarios de funcionamiento

--- analitica.py
+++ analitica.py
@@ -20,3 +20,7 @@ print(f"Usted dice que tiene {edad} años.")
 #Este analisis es indiscutiblemente erroneo
 #complementamos con mas analisis
 
+#Dockerfile.node Utilizado para instalar paquetes adicionales de node-red
+#Dockerfile.rabbit utilizado para instalar los plugins de conexion de rabbit y habilitar mqtt y mqttweb,mantencion de zona horaria zona horaria America/Bogota 
+#Dockerfile.analitica utilizada para instalar las librerias para hacer analitica que son numpy,pandas,scikit-learn,pika para la comunicacion con rabbit, scipy,paho-mqtt para la comunicacion mqtt
+#proxy.py utilizado para escuchar la cola de mensajes 
