5508eb09b458160ca0e1580110c12cc5fea177ec
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 19:24:25 -0500

    se avanza con las modificaciones

--- analitica.py
+++ analitica.py
@@ -9,3 +9,16 @@ if edad < 18:
 else:
     print("Es usted mayor de edad")
 print("¡Hasta la próxima!")
+#se avanza con las modificaciones
+edad = int(input("¿Cuántos años tiene? "))
+if edad >= 120:
+    print("¡No me lo creo!")
+print(f"Usted dice que tiene {edad} años.")
+edad = int(input("¿Cuántos años tiene? "))
+if edad < 18:
+    print("Es usted menor de edad")
+    print("Recuerde que está en la edad de aprender")
+else:
+    print("Es usted mayor de edad")
+    print("Recuerde que debe seguir aprendiendo")
+print("¡Hasta la próxima!")
