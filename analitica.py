4e4e1c7e98d07e964ff0cce443aa5323c90f602a
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 19:31:37 -0500

    analisis de muestras de servidor

--- analitica.py
+++ analitica.py
@@ -9,3 +9,12 @@ if edad < 18:
 else:
     print("Es usted mayor de edad")
 print("¡Hasta la próxima!")
+# analisis de muestras de servidor
+edad = int(input("¿Cuántos años tiene? "))
+if edad < 18:
+    print("Es usted menor de edad")
+        print("Recuerde que está en la edad de aprender")
+else:
+    print("Es usted mayor de edad")
+    print("Recuerde que debe seguir aprendiendo")
+print("¡Hasta la próxima!")
