e1c5a973906428fba7b1e9f3e24f24cb982c1882
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 19:15:33 -0500

    incio de desarrollo

--- analitica.py
+++ analitica.py
@@ -2,3 +2,11 @@ numero = int(input("Escriba un número positivo: "))
 if numero < 0:
     print("¡Le he dicho que escriba un número positivo!")
 print(f"Ha escrito el número {numero}")
+
+#Aqui comienzo a desarrollar codigo nuevo 
+if condición:
+    aquí van las órdenes que se ejecutan si la condición es cierta
+    y que pueden ocupar varias líneas
+else:
+    y aquí van las órdenes que se ejecutan si la condición es
+    falsa y que también pueden ocupar varias líneas
