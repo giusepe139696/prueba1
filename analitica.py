41c330449a6f09185b5074b5be14375df0017a4a
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-03-17 19:46:59 -0500

    limpio

--- analitica.py
+++ analitica.py
@@ -2,24 +2,6 @@ numero = int(input("Escriba un número positivo: "))
 if numero < 0:
     print("¡Le he dicho que escriba un número positivo!")
 print(f"Ha escrito el número {numero}")
-<<<<<<< HEAD
-#pruebas con respecto a errores
-edad = int(input("¿Cuántos años tiene? "))
-if edad < 18:
-    print("Es usted menor de edad")
-else:
-    print("Es usted mayor de edad")
-print("¡Hasta la próxima!")
-# analisis de muestras de servidor
-edad = int(input("¿Cuántos años tiene? "))
-if edad < 18:
-    print("Es usted menor de edad")
-        print("Recuerde que está en la edad de aprender")
-else:
-    print("Es usted mayor de edad")
-    print("Recuerde que debe seguir aprendiendo")
-print("¡Hasta la próxima!")
-=======
 
 #Aqui comienzo a desarrollar codigo nuevo 
 edad = int(input("¿Cuántos años tiene? "))
@@ -38,5 +20,3 @@ print(f"Usted dice que tiene {edad} años.")
 #Este analisis es indiscutiblemente erroneo
 #complementamos con mas analisis
 
-#cerramos y concluimos
->>>>>>> m1-server
